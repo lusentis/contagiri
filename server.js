f01c18d3636fc5c062196c85352e56eb900b0a54
Simone Lusenti <lusenti.s@gmail.com>
2017-05-31 22:07:15 +0200

    make binaries work

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const PORT = process.env.PORT || 8080;
 
 const app = express();
-app.use(express.static('build'));
+app.use(express.static(__dirname + '/build'));
 app.listen(PORT);
 
 console.log('Server ready on', `http://127.0.0.1:${PORT}`);
