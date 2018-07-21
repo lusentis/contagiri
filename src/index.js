abb4ffdd787e62a2f0a927fc98fb35a79a74e133
Simone Lusenti <lusenti.s@gmail.com>
2018-07-21 15:50:08 +0200

    serviceworker

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 import "./vendor/pure-min.css";
+import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+registerServiceWorker();
 
 console.log("App started");
